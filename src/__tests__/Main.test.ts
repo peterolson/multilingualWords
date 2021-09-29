556d43375e14729a037e82355799c8c042749927
Peter Olson <peter.e.c.olson+github@gmail.com>
2021-09-29 14:45:15 -0500

    add basic function

--- src/__tests__/Main.test.ts
+++ src/__tests__/Main.test.ts
@@ -1,5 +1,8 @@
 import { updateScore } from '../index';
 
-test('Main', () => {
+test('updateScore', () => {
   expect(updateScore(0)).toBe(0);
+  expect(() => updateScore(2)).toThrowError();
+  expect(() => updateScore(-1)).toThrowError();
+  expect(() => updateScore(NaN)).toThrowError();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export const updateScore = (score: number) => {
-  return score;
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+
+export const updateScore = (points: number, previousScore: number = 0, previousTimestamp: Date = new Date()) => {
+  if (!(points >= 0 && points <= 1)) {
+    throw new Error(`'points' must be in the interval [0,1].`);
+  }
+  const timeStampDelta = +new Date() - +previousTimestamp;
+  const days = timeStampDelta / MILLISECONDS_PER_DAY;
+  const multiplier = (points - 0.5) * 2;
+  if (multiplier >= 0) {
+    const frequencyBonus = Math.max(4 - 2 * days, 1);
+    const staticBonus = 0.01;
+    const maxIncrease = Math.max(1, previousScore);
+    return previousScore + Math.min(staticBonus + days * multiplier * frequencyBonus, maxIncrease);
+  }
+  if (previousScore <= 0) return 0;
+  const halfLife = previousScore * 0.3;
+  const diff = previousScore - previousScore * Math.pow(0.5, days / halfLife);
+  return previousScore + diff * multiplier;
 };
